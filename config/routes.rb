a7a9d3a0733f3bd05c4785a9f692c25de2804080
Luke Chadwick <me@vertis.io>
2014-06-01 10:06:02 +1000

    Make the homepage redirect to sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => "home#index"
+  root :to => redirect('/users/sign_in')
   devise_for :users, :controllers => {:registrations => "registrations"}
 
   get 'relationships', to: 'relationships#list'
